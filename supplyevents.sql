bca4fc3d52262a32f7e114970907633919b4a5e3
Jan Stoetzer <nh13jan@gmail.com>
2013-03-11 13:43:32 +0100

    Added supplyevent sql

---
+++ supplyevents.sql
@@ -0,0 +1,16 @@
+create table supplyevents (
+	id integer, 
+	event varchar(256)
+);
+
+INSERT INTO 
+	supplyevents (id, event) 
+VALUES 
+	('1', 'mfp/konica/toner/tn319k/'), 
+	('2', 'mfp/konica/toner/tn319c/'),
+	('3', 'mfp/konica/toner/tn319m/'), 
+	('4', 'mfp/konica/toner/tn319y/'),
+	
+	-- insert more stuff here
+	
+	;
